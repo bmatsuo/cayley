5d1922c10615a9e4fee66973876b100372717ec1
Bryan Matsuo <bryan.matsuo@gmail.com>
2015-10-18 17:13:56 -0700

    test suite for quadstore implementations with code generation
    The qstest and testrunner packages provide a framework for running
    generic tests on graph.QuadStore implementations.  And the accompanying
    qstest command generates tests for implementations.
    
    The command is compatible with `go generate` and generates a formatted
    source file when given a testrunner.TestRunner variable name.
    
        //go:generate -runner=Runner -output quadstore_qstest_test.go
    
    Eventually a default test runner will be built directly from a
    qstest.QuadStoreImpl.

---
+++ graph/graphtest/cmd/qstest/testfunc.go
@@ -0,0 +1,7 @@
+package main
+
+// Test is a test function for a graph.QuadStore implementation.
+type Test struct {
+	Import string
+	Name   string
+}
---
+++ graph/graphtest/qstest/qstest.go
@@ -0,0 +1,74 @@
+/*
+Package qstest provides a test framework and automatic test generation for
+graph.QuadStore implementions.  The package provides a QuadStoreImpl type that
+an implementation provides.
+*/
+package qstest
+
+import (
+	"fmt"
+
+	// TODO(bmatsuo):
+	// Change the testrunner import path if this is going to get mainlined into
+	// github.com/google/cayley.
+	"github.com/bmatsuo/cayley/graph/graphtest/testrunner"
+
+	"github.com/google/cayley/graph"
+	"golang.org/x/net/context"
+)
+
+type qsVarsKey struct{}
+
+// ContextQuadStore returns the qstest QuadStore stored in ctx or nil if there
+// is no QuadStore.
+func ContextQuadStore(ctx context.Context) graph.QuadStore {
+	vars := testrunner.ContextVars(ctx)
+	if vars == nil {
+		return nil
+	}
+	return getQuadStore(vars)
+}
+
+func getQuadStore(vs testrunner.Vars) graph.QuadStore {
+	qs, _ := vs[qsVarsKey{}].(graph.QuadStore)
+	return qs
+}
+
+func setQuadStore(vs testrunner.Vars, qs graph.QuadStore) {
+	vs[qsVarsKey{}] = qs
+}
+
+// QuadStoreImpl defines operations that allow generated tests to run against a
+// graph.QuadStore implementation..
+type QuadStoreImpl struct {
+	Name    string
+	NewArgs func(ctx context.Context, name string) (path string, opt graph.Options, err error)
+	Close   func(ctx context.Context, name string)
+}
+
+var _ testrunner.Stage = &QuadStoreImpl{}
+
+// Setup call NewArgs and creates a graph.QuadStore from the result using the call
+//		graph.NewQuadStore(impl.Name, path, opt)
+func (impl *QuadStoreImpl) Setup(ctx context.Context, name string) (err error) {
+	path, opt, err := impl.NewArgs(ctx, name)
+	if err != nil {
+		return fmt.Errorf("initializing %s: %v", name, err)
+	}
+
+	qs, err := graph.NewQuadStore(impl.Name, path, opt)
+	if err != nil {
+		impl.Teardown(ctx, name)
+		return err
+	}
+
+	vars := testrunner.ContextVars(ctx)
+	setQuadStore(vars, qs)
+
+	return nil
+}
+
+// Teardown calls impl.Close.
+func (impl *QuadStoreImpl) Teardown(ctx context.Context, name string) {
+	impl.Close(ctx, name)
+}
